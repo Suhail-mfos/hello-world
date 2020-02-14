980e143908c97df27fd68e69cdb48fa522577cba
Sohailmfos <sohailmfos@gmail.com>
2020-02-14 23:27:49 +0530

    changes done!

--- hungry.py
+++ hungry.py
@@ -1,5 +1,7 @@
-hungry = input('are you hungry?')
+hungry = input('are you hungry? ')
 if (hungry == 'yes'):
     print('eat Samosa')
 else:    
     print('Do your homework!')
+a = input('enter your name: ')
+print('hi '+ a)    
