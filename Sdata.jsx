d96cd6bb5705e7a0567b4cee1258bc07e0ba882c
Suhail <sohailmfos@gmail.com>
2020-10-09 15:02:33 +0530

    Create Sdata.jsx

---
+++ Sdata.jsx
@@ -0,0 +1,27 @@
+#   Array Data Sdata
+
+const Sdata = [
+    {
+        id: 1,
+        sname: "DARK",
+        imgsrc : "https://th.bing.com/th/id/OIP.F28Gka-cu5SNfcMBHJhR8gHaE1?pid=Api&rs=1",
+        title: " A Netflix Original Web Series ",
+        links: "https://www.netflix.com/in/title/80100172"
+    },
+    {
+        id: 2,
+        sname: "APAHARAN",
+        imgsrc : "https://www.quirkybyte.com/wp-content/uploads/2018/12/Apaharan-Web-Series-Download-1.jpg",
+        title: " An ALT BALAJI Web Series ",
+        links: "https://www.altbalaji.com/show/apharan/246"
+    },
+    {
+        id: 3,
+        sname: "MIRZAPUR",
+        imgsrc : "https://www.hindustantimes.com/rf/image_size_960x540/HT/p2/2019/02/22/Pictures/_0c75a6a4-3694-11e9-85ab-b5c6484f4b61.jpg",
+        title: " Amazon Original Web Series ",
+        links: "https://www.primevideo.com/region/eu/detail/amzn1.dv.gti.16b3417f-2d64-022e-1ab5-ff0ad034364c/ref_=dvm_pds_gen_in_as_s_gt_dsadfeedtvshow3?ref_=dvm_pds_gen_in_as_s_gt_dsaallwebpages|m_static-c_c456851804823"
+    }
+];
+
+export default Sdata;
